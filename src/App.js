ddf82f8d8c1556312780c6f5cabad67f2ec4a1e2
guiyomj <wastfg6972@naver.com>
2021-05-20 19:30:13 +0900

    210520 complete

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const App = () => {
   const [isEtc, setIsEtc] = useState(false)
+  const [isEtc2, setIsEtc2] = useState(false)
   const [deposit, setDeposit] = useState(0)
   const [monthFee, setMonthFee] = useState(0)
   const [rate, setRate] = useState(0.06)
+  const [rate2, setRate2] = useState(0.025)
   const [limit, setLimit] = useState(0.6)
   const [maxDeposit, setMaxDeposit] = useState(0)
   const [minMonthFee, setMinMonthFee] = useState(0)
+  const [maxMonthFee, setMaxMonthFee] = useState(0)
 
   const selectBtn = (e) => {
     const btn = e.target.closest('.menu-btn')
@@ -17,9 +20,23 @@ const App = () => {
       e => e.classList.remove('selected')
     )
     btn.classList.add('selected')
-    document.getElementsByClassName('content')[id].style.display = 'block'
-    document.getElementsByClassName('content')[id === 1 ? 0 : 1].style.display = 'none'
+    Array.from(document.getElementsByClassName('result')).map(e => e.style.display = 'none')
+    document.getElementsByClassName('rate')[id].style.display = 'contents'
+    document.getElementsByClassName('rate')[id === 1 ? 0 : 1].style.display = 'none'
+    document.getElementsByClassName('result')[id].style.display = 'block'
+    document.getElementsByClassName('result')[id === 1 ? 0 : 1].style.display = 'none'
   }
+
+  const calcMax = () => {
+    setMaxDeposit(Math.floor(monthFee * limit / rate * 12 / 100000) * 100000)
+    setMinMonthFee(maxDeposit * rate / 12)
+  }
+
+  const calcMin = () => {
+    setMaxMonthFee(Math.floor(deposit * rate2 / 100000 / 3) * 100000 / 4)
+  }
+
+  useEffect(() => calcMax() || calcMin())
 
   return (
     <div className="container">
@@ -36,71 +53,90 @@ const App = () => {
           <small>(월세를 최대로)</small>
         </li>
       </ul>
-      <div className="content">
-        <table className="c-table">
-          <colgroup>
-            <col width="90"/>
-            <col width="*"/>
-          </colgroup>
-          <tbody>
-            <tr>
-              <th>기본보증금</th>
-              <td><input type="number" onChange={e => setDeposit(Number(e.target.value))} placeholder="기본 보증금을 입력하세요. ex) 2000000" /></td>
-            </tr>
-            <tr>
-              <th>기본월세</th>
-              <td><input type="number" onChange={e => setMonthFee(Number(e.target.value))} placeholder="기본 월세를 입력하세요. ex) 150000" /></td>
-            </tr>
-            <tr>
-              <th>전환이율{rate}{Number(isEtc)}</th>
-              <td>
-                <div className="radioGroup">
-                  <label onClick={e => setRate(Number(e.target.value)) && setIsEtc(false) }>
-                    <input name="rate" type="radio" value="0.06" defaultChecked /> 6%
+      <table className="c-table">
+        <colgroup>
+          <col width="90"/>
+          <col width="*"/>
+        </colgroup>
+        <tbody>
+          <tr>
+            <th>기본보증금</th>
+            <td><input type="number" id="deposit" onChange={e => {setDeposit(Number(e.target.value))}} placeholder="기본 보증금을 입력하세요. ex) 2000000" /></td>
+          </tr>
+          <tr>
+            <th>기본월세</th>
+            <td><input type="number" onChange={e => setMonthFee(Number(e.target.value))} placeholder="기본 월세를 입력하세요. ex) 150000" /></td>
+          </tr>
+          <tr className="rate">
+            <th>전환이율</th>
+            <td>
+              <div className="radioGroup">
+                <label onClick={e => setRate(Number(e.target.value)) || setIsEtc(false) }>
+                  <input name="rate" type="radio" value="0.06" defaultChecked /> 6%
+                </label>
+                <div className="d-flex">
+                  <label onClick={() => setIsEtc(true) || setRate(document.getElementsByClassName('etc-text')[0].value === ''? 0 : document.getElementsByClassName('etc-text')[0].value)}>
+                    <input name="rate" type="radio" /> 기타
                   </label>
-                  <div className="d-flex">
-                    <label onClick={() => setRate(0) && setIsEtc(true) && console.log("---",document.getElementsByClassName('etc-text')[0].value)}>
-                      <input name="rate" type="radio" /> 기타
-                    </label>
-                    <input type="number" className="etc-text" placeholder="퍼센트(%)를 입력하세요." disabled={!isEtc && 'disabled'} onChange={e => setRate(Number(e.target.value / 100))}  />%
-                  </div>
+                  <input type="number" className="etc-text" placeholder="퍼센트(%)를 입력하세요." disabled={!isEtc && 'disabled'} onChange={e => setRate(Number(e.target.value / 100))}  />%
                 </div>
-              </td>
-            </tr>
-            <tr>
-              <th>최대전환비율</th>
-              <td>
-                <div className="radioGroup">
-                  <label onClick={e => setLimit(Number(e.target.value))}>
-                    <input name="limit" type="radio" value="0.6" defaultChecked /> 60%
+              </div>
+            </td>
+          </tr>
+          <tr className="rate d-none">
+            <th>전환이율</th>
+            <td>
+              <div className="radioGroup">
+                <label onClick={e => setRate2(Number(e.target.value)) || setIsEtc2(false) }>
+                  <input name="rate2" type="radio" value="0.025" defaultChecked /> 2.5%
+                </label>
+                <div className="d-flex">
+                  <label onClick={() => setIsEtc2(true) || setRate2(document.getElementsByClassName('etc-text2')[0].value === ''? 0 : document.getElementsByClassName('etc-text2')[0].value)}>
+                    <input name="rate2" type="radio" /> 기타
                   </label>
-                  <label onClick={e => setLimit(Number(e.target.value))}>
-                    <input name="limit" type="radio" value="0.7" /> 70%
-                  </label>
-                  <label onClick={e => setLimit(Number(e.target.value))}>
-                    <input name="limit" type="radio" value="0.8" /> 80%
-                  </label>
+                  <input type="number" className="etc-text2" placeholder="퍼센트(%)를 입력하세요." disabled={!isEtc2 && 'disabled'} onChange={e => setRate2(Number(e.target.value / 100))}  />%
                 </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <button className="result-btn" type="button" onClick={ () => { document.getElementsByClassName('result')[0].style.display = 'block'; setMaxDeposit(Math.floor(monthFee * limit / rate * 12 / 100000) * 100000); setMinMonthFee(maxDeposit * rate / 12) } }>결과 보기</button>
-        <div className="result">
-          <p></p>
-          <span>결과</span>
-          <div className="result-content">
-            <p>
-              최대 전환가능 보증금은 <b>{maxDeposit}</b>원(총 보증금 {maxDeposit + deposit}원)이고,<br />
-              낮아지는 월 임대료는 <b>{minMonthFee}</b>원(총 월세 {monthFee - minMonthFee}원)<br />
-              입니다.
-            </p>
-          </div>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <th>최대전환비율</th>
+            <td>
+              <div className="radioGroup">
+                <label onClick={e => setLimit(Number(e.target.value))}>
+                  <input name="limit" type="radio" value="0.6" defaultChecked /> 60%
+                </label>
+                <label onClick={e => setLimit(Number(e.target.value))}>
+                  <input name="limit" type="radio" value="0.7" /> 70%
+                </label>
+                <label onClick={e => setLimit(Number(e.target.value))}>
+                  <input name="limit" type="radio" value="0.8" /> 80%
+                </label>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <div className="result">
+        <p></p>
+        <span>결과</span>
+        <div className="result-content">
+          <p>
+            최대 전환가능 보증금은 <b>{maxDeposit}</b>원(총 보증금 {maxDeposit + deposit}원)이고,<br />
+            낮아지는 월 임대료는 <b>{minMonthFee}</b>원(총 월세 {monthFee - minMonthFee}원)<br />
+            입니다.
+          </p>
         </div>
       </div>
-      <div className="content">
-        2번쨰
-        <p className="result">결과</p>
+      <div className="result d-none">
+        <p></p>
+        <span>결과</span>
+        <div className="result-content">
+          <p>
+            무보증금 기준 높아지는 월 임대료는 <b>{maxMonthFee}</b>원(총 월세 {monthFee + maxMonthFee}원)<br />
+            입니다.
+          </p>
+        </div>
       </div>
     </div>
   );
